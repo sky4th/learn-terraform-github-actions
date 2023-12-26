6c970044b30fbb85232793be164a50ca776f95d1
sky4th <88715446+sky4th@users.noreply.github.com>
2023-12-26 17:32:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,31 @@
 #############################################################################
-# Resource Group
+# key vault 
 #################################################################################
-resource "azurerm_resource_group" "hubrg" {
-  name     = var.resource_group_name
-  location = var.location
+resource "azurerm_key_vault" "example" {
+  name                        = "examplekeyvault-sky"
+  location                    = westeurope
+  resource_group_name         = "hub-rg"
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
 }
